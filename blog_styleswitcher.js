fb30bd5ad2eab47446e65840df7daa01b50b0d3f
Tino <tinomuzambi@gmail.com>
2020-04-03 23:09:55 +0200

    Cleaning up.

--- blog_styleswitcher.js
+++ blog_styleswitcher.js
@@ -3,21 +3,15 @@ function swapStyleSheet(sheet) {
 }
 
 function updateB() {
-    console.log(dark)
     const style1 = document.getElementById("switcher2");
     if (sessionStorage.getItem("dark") === "true") {
         style1.innerHTML = "LIGHT";
         swapStyleSheet("dark.css");
-        dark = true;
-        console.log("dark true in blog")
     }
     else {
         style1.innerHTML = "DARK";
         swapStyleSheet("light.css");
-        dark = false
-        console.log("dark false in blog")
     }
-    console.log(dark)
 
     initiate()
 }
@@ -29,15 +23,11 @@ function initiate() {
         if (sessionStorage.getItem("dark") !== "true") {
             style1.innerHTML = "LIGHT";
             swapStyleSheet("dark.css");
-            dark = true;
-            console.log("dark true in blog")
             sessionStorage.setItem("dark","true");
         }
         else {
             style1.innerHTML = "DARK";
             swapStyleSheet("light.css");
-            dark = false;
-            console.log("dark false in blog")
             sessionStorage.setItem("dark","false");
         }
     };
--- index_styleswitcher.js
+++ index_styleswitcher.js
@@ -1,23 +1,16 @@
-let dark = false;
-
 function swapStyleSheet(sheet) {
     document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
 function update() {
-    console.log(sessionStorage.getItem("dark"));
     const style1 = document.getElementById("switcher1");
     if (sessionStorage.getItem("dark") === "true") {
         style1.innerHTML = "LIGHT";
         swapStyleSheet("dark.css");
-        dark = true;
-        console.log("dark true in index")
     }
     else {
         style1.innerHTML = "DARK";
         swapStyleSheet("light.css");
-        dark = false
-        console.log("dark false in index")
     }
 
     initiate()
@@ -30,15 +23,11 @@ function initiate() {
         if (sessionStorage.getItem("dark") !== "true") {
             style1.innerHTML = "LIGHT";
             swapStyleSheet("dark.css");
-            dark = true;
-            console.log("dark true in index")
             sessionStorage.setItem("dark","true");
         }
         else {
             style1.innerHTML = "DARK";
             swapStyleSheet("light.css");
-            dark = false
-            console.log("dark false in index")
             sessionStorage.setItem("dark","false");
         }
     };
